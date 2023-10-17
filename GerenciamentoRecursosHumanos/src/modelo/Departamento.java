f812129dda1034e8e9715e8444a33d53c2bb20bb
Wesley <Wesley@DESKTOP-BH6M3VH>
2023-10-16 21:20:58 -0300

    Inplementação da classe Departamento

--- GerenciamentoRecursosHumanos/src/modelo/Departamento.java
+++ GerenciamentoRecursosHumanos/src/modelo/Departamento.java
@@ -1,7 +1,100 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Departamento {
+	private int id;
+	private String nome;
+	private String descricao;
+	private List<Funcionario> funcionarios; // Lista de funcionários no departamento
+	private Funcionario responsavel; // Funcionário responsável pelo departamento
+	private String descricaoDepartamento;
 	private String nomeDoDepartamento;
-	private String descricricaoDoDepartamento;
-	
+
+	public Departamento(int id, String nome, String descricao) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.funcionarios = new ArrayList<>();
+	}
+
+	@Override
+	public String toString() {
+		return "Departamento [id=" + id + ", nome=" + nome + ", descricao=" + descricao + ", funcionarios="
+				+ funcionarios + ", responsavel=" + responsavel + ", descricaoDepartamento=" + descricaoDepartamento
+				+ ", nomeDoDepartamento=" + nomeDoDepartamento + "]";
+	}
+
+	// Adicionar um funcionário ao departamento
+	public void adicionarFuncionario(Funcionario funcionario) {
+		funcionarios.add(funcionario);
+	}
+
+	// Remover um funcionário do departamento
+	public void removerFuncionario(Funcionario funcionario) {
+		funcionarios.remove(funcionario);
+	}
+
+	// Definir o responsável pelo departamento
+	public void definirResponsavel(Funcionario funcionario) {
+		this.responsavel = funcionario;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+	public Funcionario getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(Funcionario responsavel) {
+		this.responsavel = responsavel;
+	}
+
+	public String getDescricaoDepartamento() {
+		return descricaoDepartamento;
+	}
+
+	public void setDescricaoDepartamento(String descricaoDepartamento) {
+		this.descricaoDepartamento = descricaoDepartamento;
+	}
+
+	public String getNomeDoDepartamento() {
+		return nomeDoDepartamento;
+	}
+
+	public void setNomeDoDepartamento(String nomeDoDepartamento) {
+		this.nomeDoDepartamento = nomeDoDepartamento;
+	}
+
 }
